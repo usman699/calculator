4f628d0a8c7de5b2cbadff99229af55ee60059e7
usman699 <musmangulzar93@gmail.com>
2024-11-28 09:47:42 +0500

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Spark M1 on 28/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

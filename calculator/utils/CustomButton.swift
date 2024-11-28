921841105c4f2a249aa9382c90084008e3874482
usman699 <musmangulzar93@gmail.com>
2024-11-28 16:55:00 +0500

    added custom button

---
+++ calculator/utils/CustomButton.swift
@@ -0,0 +1,18 @@
+//
+//  CustomButton.swift
+//  calculator
+//
+//  Created by Spark M1 on 28/11/2024.
+//
+
+import SwiftUI
+
+struct CustomButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomButton()
+}

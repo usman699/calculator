7f5bc9ce6cd1e652d5b13a27423cdd2b7477e70b
usman699 <musmangulzar93@gmail.com>
2024-11-29 11:27:42 +0500

    all layout is done

--- calculator/ContentView.swift
+++ calculator/ContentView.swift
@@ -1,58 +1,115 @@
-//
-//  ContentView.swift
-//  calculator
-//
-//  Created by Spark M1 on 28/11/2024.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    
+    let numberOfRows: CGFloat = 5
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        
+    ]
+    
     var body: some View {
-        VStack {
-            
-            
-            
-            
-            
-            Grid{
-                GridRow {
-                    ForEach(1..<5) { i in
-                        
-                        CustomButton(title1: i.description,  action: {
+       
+       // Set spacing to 0 for compact layout
+            VStack {
+               
+                Color.black.opacity(0.8)
+                GeometryReader { geo in
+                    
+                    
+                    LazyVGrid(columns: columns) {
+                    
+                    
+                        CustomButton(title1: "AC", action: {}, background: Color.secondary, forgroundColor: Color.white)
+                        CustomButton(title1: "+/-", action: {}, background: Color.secondary, forgroundColor: Color.white)
+                        CustomButton(title1: "%", action: {}, background: Color.secondary, forgroundColor: Color.white)
+                        CustomButton(title1: "÷", action: {}, background: Color.secondary, forgroundColor: Color.white)
+                        ForEach(7..<10) { i in
+                            CustomButton(title1: i.description, action: {}, background: Color.secondary.opacity(0.8), forgroundColor: Color.white)
                         }
-                                     ,background: Color.gray
-                                     ,forgroundColor: Color.white
-                        )
-                    }
-                    
-                    
-                    }
-                    
-                GridRow {
-                    ForEach(4..<8) { i in
-                        
-                        CustomButton(title1: i.description,  action: {
+                        CustomButton(title1: "x", action: {}, background: Color.orange, forgroundColor: Color.white)
+          
+                        ForEach(4..<7) { i in
+                            CustomButton(title1: i.description, action: {}, background: Color.secondary.opacity(0.8), forgroundColor: Color.white)
                         }
-                                     ,background: Color.gray
-                                     ,forgroundColor: Color.white
-                        )
+                        CustomButton(title1: "-", action: {}, background: Color.orange, forgroundColor: Color.white)
+                 
+                        ForEach(1..<4) { i in
+                            CustomButton(title1: i.description, action: {}, background: Color.secondary.opacity(0.8), forgroundColor: Color.white)
+                        }
+                        CustomButton(title1: "+", action: {}, background: Color.orange, forgroundColor: Color.white)
+                        
+                        CustomButtonlarge(title1: "0", action: {}, background: Color.secondary.opacity(0.8), forgroundColor: Color.white)
+                            .frame(width: geo.size.width * 0.5 - 15, height: 60)
+                           
+                                    
+                        CustomButton(title1: ".", action: {}, background: Color.secondary.opacity(0.8), forgroundColor: Color.white)
+                        CustomButton(title1: "=", action: {}, background: Color.orange, forgroundColor: Color.white)
+                        
+                        
+                        
+                        
+                        
                     }
-                    
-                    
-                    }
-                    
                     
                 }
-                
-                
-                
-                
-                
-                
-            }
-        }}
-#Preview {
-    ContentView()
-}
+                            
+                        }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                 .background(Color.black)
+                 .ignoresSafeArea(edges: .bottom)
+                    }
+        
+                }
+    
+    
 
+
+//    Grid {
+//        // First row
+//        GridRow {
+//            CustomButton(title1: "AC", action: {}, background: Color.secondary, forgroundColor: Color.white)
+//            CustomButton(title1: "+/-", action: {}, background: Color.secondary, forgroundColor: Color.white)
+//            CustomButton(title1: "%", action: {}, background: Color.secondary, forgroundColor: Color.white)
+//            CustomButton(title1: "÷", action: {}, background: Color.orange, forgroundColor: Color.white)
+//        }
+//        
+//        // Second row
+//        GridRow {
+//            ForEach(7..<10) { i in
+//                CustomButton(title1: i.description, action: {}, background: Color.gray.opacity(0.8), forgroundColor: Color.white)
+//            }
+//            CustomButton(title1: "x", action: {}, background: Color.orange, forgroundColor: Color.white)
+//        }
+//        
+//        // Third row
+//        GridRow {
+//            ForEach(4..<7) { i in
+//                CustomButton(title1: i.description, action: {}, background: Color.gray.opacity(0.8), forgroundColor: Color.white)
+//            }
+//            CustomButton(title1: "-", action: {}, background: Color.orange, forgroundColor: Color.white)
+//        }
+//        
+//        // Fourth row
+//        GridRow {
+//            ForEach(1..<4) { i in
+//                CustomButton(title1: i.description, action: {}, background: Color.gray.opacity(0.8), forgroundColor: Color.white)
+//            }
+//            CustomButton(title1: "+", action: {}, background: Color.orange, forgroundColor: Color.white)
+//        }
+//        
+//        // Last row with "0" spanning two columns
+//        GridRow {
+//            CustomButton(title1: "0", action: {}, background: Color.gray.opacity(0.8), forgroundColor: Color.white)
+//                .gridCellColumns(2) // Span 2 columns
+//            CustomButton(title1: ".", action: {}, background: Color.gray.opacity(0.8), forgroundColor: Color.white)
+//            CustomButton(title1: "=", action: {}, background: Color.orange, forgroundColor: Color.white)
+//        }
+//    }
+//    .frame(maxWidth: .infinity, maxHeight: .infinity) // Ensure grid takes full screen
+//}
+//.ignoresSafeArea(edges: .bottom) // Ignore bottom safe area for full screen
+//.background(Color.black) // Optional: Set back
--- calculator/utils/CustomButton.swift
+++ calculator/utils/CustomButton.swift
@@ -19,3 +19,23 @@ struct CustomButton :View {
         .font(. system(size: 22))
     }
 }
+
+struct CustomButtonlarge :View {
+    var title1:String
+    var action:(() -> Void)
+    var background:Color
+    var forgroundColor :Color
+    var body: some View {
+        Button(action: action){
+            HStack{
+                Text(title1)
+            }
+        }
+        
+        .frame(width: UIScreen.main.bounds.width * 0.5 ,height: 60)
+        .background(background)
+        .foregroundStyle(forgroundColor)
+        .clipShape(Circle())
+        .font(. system(size: 22))
+    }
+}

5e50aedba3b5c1db32d475f502a2a58a95c83e7e
usman699 <musmangulzar93@gmail.com>
2024-12-03 10:15:10 +0500

    new things

---
+++ calculator/layout.swift
@@ -0,0 +1,18 @@
+//
+//  layout.swift
+//  calculator
+//
+//  Created by Spark M1 on 02/12/2024.
+//
+
+import SwiftUI
+
+struct layout: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    layout()
+}
